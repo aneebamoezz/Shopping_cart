4ad5e950341b5c3b2ac571ef4e46b5c8d1debd73
aneebamoezz <anibakaleem@gmail.com>
2024-04-28 21:41:44 +0500

    commit initial files

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/cart" element={<Cart />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
